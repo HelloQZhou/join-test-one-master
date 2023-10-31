40541477b196f6c0a4637132d37415fb9136eee7
QZone <2024096103@qq.com>
2023-10-31 15:11:49 +0800

    删除

--- ManagementProject/src/main/java/com/yizhi/common/config/SpringSessionConfig.java
+++
@@ -1,8 +0,0 @@
-package com.yizhi.common.config;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-
-@ConditionalOnProperty(prefix = "yizhi", name = "spring-session-open", havingValue = "true")
-public class SpringSessionConfig {
-
-}
--- ManagementProject/src/main/java/com/yizhi/common/utils/Base64Utils.java
+++
@@ -1,5 +0,0 @@
-package com.yizhi.common.utils;
-
-public class Base64Utils {
-	
-}
